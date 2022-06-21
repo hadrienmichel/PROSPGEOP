c0be219ccfea5da775def5d1e21ea1a95904c174
Hadrien Michel <hadrien.michel@uliege.be>
2022-06-21 11:31:21 +0200

    Using geopandas in EMi to better convert to shp

--- EMI/SortingEMI.py
+++ EMI/SortingEMI.py
@@ -2,32 +2,45 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot
+try:
+    geoPandasImport = True
+    import geopandas as gpd
+except:
+    geoPandasImport = False
 from scipy import stats
 from pathlib import Path
 # Lire le fichier de données
-filename = Path('./data/exemple.dat') # Changer le fichier a lire et le trajet ICI
+filename = Path("./EMI/data/EMI_Colonster_TP_Geophy.dat") # Changer le fichier a lire et le trajet ICI
 data = pd.read_csv(filename, delimiter='\t', header=0, index_col=False)
-nbInit = len(data.index)
+if geoPandasImport:
+    geoData = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.Longitude, data.Latitude, data.Altitude), crs='epsg:4326')
+    geoData = geoData.to_crs('epsg:31370') # Convert to Lambert 72
+else:
+    geoData = data
+nbInit = len(geoData.index)
 print('Initial number of values: {}'.format(nbInit))
 # Montrer les histograms initiaux:
-hist = data.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
+hist = geoData.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
 pyplot.show()
 # Supprimer les données non-physique (négatives) en utilisant la méthode "ge" (greater or equal) de pandas:
-data = data[data[['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]']].ge(0).all(1)]
-nbInit = len(data.index)
+geoData = geoData[geoData[['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]']].ge(0).all(1)]
+nbInit = len(geoData.index)
 print('Number of physical values: {}'.format(nbInit))
 # Montrer les histogrammes de chaque paramètre:
-hist = data.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
+hist = geoData.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
 pyplot.show()
 # On voit que les histograms ont des ranges beaucoup trop large. C'est du a la présence d'outliers.
 # On peut les détecter simplement par mesure statistique (écart par rapport a la moyenne - z-score) par exemple
-data = data[(np.abs(stats.zscore(data[['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]']]))<3).all(axis=1)]
-nbInit = len(data.index)
+geoData = geoData[(np.abs(stats.zscore(geoData[['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]']]))<3).all(axis=1)]
+nbInit = len(geoData.index)
 print('Number of in-range values: {}'.format(nbInit))
 # Vérifier visuellement que les données fausses soient bien enlevées:
-hist = data.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
+hist = geoData.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
 pyplot.show()
 # Sauver les données au format csv pour QGIS (séparateur=tabulation):
 filenameSave = filename.parents[0] / (filename.stem + '_sorted' + filename.suffix)
-data.to_csv(filenameSave, sep='\t', index=False)
+geoData.to_csv(filenameSave, sep='\t', index=False)
+if geoPandasImport:
+    filenameSaveSHP = filename.parents[0] / (filename.stem + '_sorted.shp')
+    geoData.to_file(filenameSaveSHP)
 print('File saved successfully with the changes !')
