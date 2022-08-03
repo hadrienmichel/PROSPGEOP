9079e32ae8ebbbb84c944dc6cbc43c68ef87abbd
Hadrien Michel <hadrien.michel@uliege.be>
2022-08-03 13:23:36 +0200

    improvements of the emi sorter

--- EMI/SortingEMI.py
+++ EMI/SortingEMI.py
@@ -1,10 +1,12 @@
 # Dans ce script, nous allons trier un jeux de données fourni: "exemple.dat"
 import numpy as np
+import math as mt
 import pandas as pd
 from matplotlib import pyplot
 try:
     geoPandasImport = True
     import geopandas as gpd
+    from osgeo import gdal
 except:
     geoPandasImport = False
 from scipy import stats
@@ -21,14 +23,14 @@ nbInit = len(geoData.index)
 print('Initial number of values: {}'.format(nbInit))
 # Montrer les histograms initiaux:
 hist = geoData.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
-pyplot.show()
+pyplot.show(block=False)
 # Supprimer les données non-physique (négatives) en utilisant la méthode "ge" (greater or equal) de pandas:
 geoData = geoData[geoData[['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]']].ge(0).all(1)]
 nbInit = len(geoData.index)
 print('Number of physical values: {}'.format(nbInit))
 # Montrer les histogrammes de chaque paramètre:
 hist = geoData.hist(column=['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]'])
-pyplot.show()
+pyplot.show(block=False)
 # On voit que les histograms ont des ranges beaucoup trop large. C'est du a la présence d'outliers.
 # On peut les détecter simplement par mesure statistique (écart par rapport a la moyenne - z-score) par exemple
 geoData = geoData[(np.abs(stats.zscore(geoData[['Cond.1[mS/m]','Inph.1[ppt]','Cond.2[mS/m]','Inph.2[ppt]','Cond.3[mS/m]','Inph.3[ppt]']]))<3).all(axis=1)]
@@ -43,4 +45,26 @@ geoData.to_csv(filenameSave, sep='\t', index=False)
 if geoPandasImport:
     filenameSaveSHP = filename.parents[0] / (filename.stem + '_sorted.shp')
     geoData.to_file(filenameSaveSHP)
+    bounds = np.round(geoData.total_bounds)
+    deltaX = round(bounds[2] - bounds[0])
+    deltaY = round(bounds[3] - bounds[1])
+    delta = 2.5
+    width = mt.ceil(deltaX/delta)
+    height = mt.ceil(deltaY/delta)
+    midPointX = (bounds[2] + bounds[0])/2
+    midPointY = (bounds[3] + bounds[1])/2
+    bounds = [midPointX-width/2*delta, 
+              midPointY-height/2*delta,
+              midPointX+width/2*delta,
+              midPointY+height/2*delta]
+
+    raster = gdal.Grid(str('./EMI/data/Interpolated.tif'), 
+                       str(filenameSaveSHP),
+                       format="GTiff",
+                       algorithm='invdist',
+                       width=width,
+                       height=height,
+                       outputBounds=bounds,
+                       zfield='Cond.3[mS/')
+    raster.FlushCache()
 print('File saved successfully with the changes !')
